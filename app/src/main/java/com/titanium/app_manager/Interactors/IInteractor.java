fbd82b8db98cf1f234ecc08bfdd0d397cd82ec75
MrKovalev <46102657+MrKovalev@users.noreply.github.com>
2019-02-12 18:02:27 +0300

    clear repo with garbage
    Очистка репозитория. Следующим коммитом полный перезалив проекта, очищенного от лишних файлов.

--- app/src/main/java/com/titanium/app_manager/Interactors/IInteractor.java
+++
@@ -1,9 +0,0 @@
-package com.titanium.app_manager.Interactors;
-
-import com.titanium.app_manager.Data.Model.AppInfo;
-
-import java.util.List;
-
-public interface IInteractor {
-    List<AppInfo> filterApps(int currentList, List<AppInfo> mAppsList);
-}
--- app/src/main/java/com/titanium/app_manager/Utils/RequestAndResultCodes.java
+++
@@ -1,8 +0,0 @@
-package com.titanium.app_manager.Utils;
-
-public final class RequestAndResultCodes {
-    public static final int REQUEST_UNINSTALL = 1;
-
-    public static final int RESULT_OK = -1;
-    public static final int RESULT_CANCEL = 0;
-}
